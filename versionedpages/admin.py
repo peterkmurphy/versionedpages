975e04a24261140a97ce8c86be244545e5b0e60a
Peter Murphy <peterkmurphy@gmail.com>
2018-04-06 22:13:38 +1000

    Add redirection to Admin page model

--- versionedpages/admin.py
+++ versionedpages/admin.py
@@ -1,19 +1,20 @@
 from django.contrib import admin
 from mezzanine.pages.admin import PageAdmin
 from .models import VersionPage #, VersionPageRevision
-#from mezzanine.core.admin import StackedDynamicInlineAdmin
-#
-#class VersionPageRevisionInline(StackedDynamicInlineAdmin):
-#    model = VersionPageRevision
-#    extra = 0
-#class VersionPageAdmin(PageAdmin):
-#    inlines = (VersionPageRevisionInline,)
-#
-#
-#admin.site.register(VersionPage, VersionPageAdmin)
+from django.shortcuts import redirect
+
 from reversion_compare.admin import CompareVersionAdmin
 
 class VersionPageAdmin(CompareVersionAdmin):
     pass
+
+
+# Methods for Django 1.10 and Mezzanine 4.0. Subject to change.
+
+    def response_add(self, request, obj, post_url_continue=None):
+        return redirect('/admin/pages/page/')
+
+    def response_change(self, request, obj):
+        return redirect('/admin/pages/page/')
 
 admin.site.register(VersionPage, VersionPageAdmin)
